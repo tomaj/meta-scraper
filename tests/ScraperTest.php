d5090e0ab2cfabb1b64012522ffc3e8455fd04e0
Tomas Majer <tomasmajer@gmail.com>
2015-10-18 09:56:58 +0200

    Fixed indentation in test

--- tests/ScraperTest.php
+++ tests/ScraperTest.php
@@ -2,44 +2,44 @@
 
 namespace Tomaj\Scraper;
 
-use PHPUnit_Framework_TestCase;
-
 require dirname(__FILE__). '/../vendor/autoload.php';
+
+use PHPUnit_Framework_TestCase;
 
 class TestScraper extends PHPUnit_Framework_TestCase
 {
     public function testBasicInformation()
     {
-      $data = <<<EOT
-          <title>Page title</title>
-          <meta name="description" content="Default page description"/>
-          <meta name="KEYWORDS"     content="Keyword1,Keyword2">
-          <META NAME="author" CONTENT="Jozko Pucik"  >
-          <meta property="og:type" content="article" />
-          <meta property="og:description" content="Silny popis" />
-          <meta property="og:url" content="https://web.sk/stranka.html"/>
-          <meta property="og:site_name" content="Mega site name" />
-          <meta property="article:section" content="Ekonomika" />
-          <meta property="og:image" content="https://obrazok.jpg">
-          <meta property="article:published_time" content="2015-10-12T12:40:27+00:00" />
-          <meta property="article:modified_time" content="2016-11-13T13:21:42+00:00" />
+        $data = <<<EOT
+            <title>Page title</title>
+            <meta name="description" content="Default page description"/>
+            <meta name="KEYWORDS"     content="Keyword1,Keyword2">
+            <META NAME="author" CONTENT="Jozko Pucik"  >
+            <meta property="og:type" content="article" />
+            <meta property="og:description" content="Silny popis" />
+            <meta property="og:url" content="https://web.sk/stranka.html"/>
+            <meta property="og:site_name" content="Mega site name" />
+            <meta property="article:section" content="Ekonomika" />
+            <meta property="og:image" content="https://obrazok.jpg">
+            <meta property="article:published_time" content="2015-10-12T12:40:27+00:00" />
+            <meta property="article:modified_time" content="2016-11-13T13:21:42+00:00" />
 EOT;
 
-      $scraper = new Scraper();
-      $meta = $scraper->parse($data);
+        $scraper = new Scraper();
+        $meta = $scraper->parse($data);
 
-      $this->assertEquals('Page title', $meta->getTitle());
-      $this->assertEquals('Default page description', $meta->getDescription());
-      $this->assertEquals('Jozko Pucik', $meta->getAuthor());
+        $this->assertEquals('Page title', $meta->getTitle());
+        $this->assertEquals('Default page description', $meta->getDescription());
+        $this->assertEquals('Jozko Pucik', $meta->getAuthor());
 
-      $this->assertEquals('article', $meta->getOgType());
-      $this->assertEquals('Silny popis', $meta->getOgDescription());
-      $this->assertEquals('https://web.sk/stranka.html', $meta->getOgUrl());
-      $this->assertEquals('Mega site name', $meta->getOgSiteName());
-      $this->assertEquals('https://obrazok.jpg', $meta->getOgImage());
-      $this->assertEquals('Ekonomika', $meta->getSection());
-      $this->assertEquals('12.10.2015 12:40:27', $meta->getPublishedTime()->format('d.m.Y H:i:s'));
-      $this->assertEquals('13.11.2016 13:21:42', $meta->getModifiedTime()->format('d.m.Y H:i:s'));
+        $this->assertEquals('article', $meta->getOgType());
+        $this->assertEquals('Silny popis', $meta->getOgDescription());
+        $this->assertEquals('https://web.sk/stranka.html', $meta->getOgUrl());
+        $this->assertEquals('Mega site name', $meta->getOgSiteName());
+        $this->assertEquals('https://obrazok.jpg', $meta->getOgImage());
+        $this->assertEquals('Ekonomika', $meta->getSection());
+        $this->assertEquals('12.10.2015 12:40:27', $meta->getPublishedTime()->format('d.m.Y H:i:s'));
+        $this->assertEquals('13.11.2016 13:21:42', $meta->getModifiedTime()->format('d.m.Y H:i:s'));
     }
 
     public function testEmpty()
