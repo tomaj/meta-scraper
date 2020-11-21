da49475949931778111ef0460ed060839eebd919
Pavel Gradoboyev <flashworking@gmail.com>
2020-11-21 22:21:32 +0300

    add reverse attributes position test for OgDomParser

--- tests/ScraperTest.php
+++ tests/ScraperTest.php
@@ -298,4 +298,42 @@ EOT;
         $this->assertEquals('12.10.2015 12:40:27', $meta->getPublishedTime()->format('d.m.Y H:i:s'));
         $this->assertEquals('13.11.2016 13:21:42', $meta->getModifiedTime()->format('d.m.Y H:i:s'));
     }
+
+    public function testReverseAttrPositionOgDomParser()
+    {
+        $data = <<<EOT
+            <title>Page title</title>
+            <meta content="Default page description" name="description"/>
+            <meta content="Keyword1,Keyword2" name="KEYWORDS">
+            <META CONTENT="Jozko Pucik"   NAME="author">
+            <meta content="article" property="og:type" />
+            <meta content="Silny popis" property="og:description" />
+            <meta content="Og title nadpis" property="og:title" />
+            <meta content="https://web.sk/stranka.html" property="og:url" />
+            <meta content="Mega site name" property="og:site_name" />
+            <meta content="Ekonomika" property="article:section" />
+            <meta content="https://obrazok.jpg" property="og:image" />
+            <meta content="2015-10-12T12:40:27+00:00" property="article:published_time" />
+            <meta content="2016-11-13T13:21:42+00:00" property="article:modified_time" />
+EOT;
+
+        $scraper = new Scraper();
+        $meta    = $scraper->parse($data, [new OgDomParser()]);
+
+        $this->assertEquals('Page title', $meta->getTitle());
+
+        $this->assertEquals('Default page description', $meta->getDescription());
+        $this->assertEquals('Keyword1,Keyword2', $meta->getKeywords());
+        $this->assertEquals([new Author(null, 'Jozko Pucik')], $meta->getAuthors());
+
+        $this->assertEquals('article', $meta->getOgType());
+        $this->assertEquals('Silny popis', $meta->getOgDescription());
+        $this->assertEquals('https://web.sk/stranka.html', $meta->getOgUrl());
+        $this->assertEquals('Mega site name', $meta->getOgSiteName());
+        $this->assertEquals('Og title nadpis', $meta->getOgTitle());
+        $this->assertEquals('https://obrazok.jpg', $meta->getOgImage());
+        $this->assertEquals([new Section(null, 'Ekonomika')], $meta->getSections());
+        $this->assertEquals('12.10.2015 12:40:27', $meta->getPublishedTime()->format('d.m.Y H:i:s'));
+        $this->assertEquals('13.11.2016 13:21:42', $meta->getModifiedTime()->format('d.m.Y H:i:s'));
+    }
 }
